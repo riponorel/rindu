7de5da6d0c47aac1d04f9d0fc59c25d0aba49e60
riponorel <riponorel@gmail.com>
2024-11-25 21:38:58 +0700

    Update run-t.py

--- run-t.py
+++ run-t.py
@@ -69,8 +69,8 @@ def run_bot(data_account, recover=1):
         nama_modif = kw.replace(" ", "-")
         gmail = f"{nama_modif}-viral-vidioo-{random_string(6)}@gmail.com"
         slug = f"{nama_modif}-onlyfanss-{random_string(6)}"
-        judul = f"[~𝐄𝐗𝐂𝐋𝐔𝐒𝐈𝐕𝐄~]** {kw} Onlyfans Leaked Video Viral On Social Media Twitter x And Tiktok Trending Now"
-        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25B"
+        judul = f"[-TRENDING-]— {kw} Onlyfans Video Original Link {kw} Video Viral On Social Media X Trending Now"
+        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25C"
 
         driver.get("https://bento.me/signup?ref=techcrunch&app=wetransferflow&atb=true")
         time.sleep(3)
