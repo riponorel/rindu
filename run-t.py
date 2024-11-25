2ca33a66ff5d31b213f4ce9e9f51842e212a3794
riponorel <riponorel@gmail.com>
2024-11-25 10:15:50 +0700

    Update run-t.py

--- run-t.py
+++ run-t.py
@@ -68,9 +68,9 @@ def run_bot(data_account, recover=1):
 
         nama_modif = kw.replace(" ", "-")
         gmail = f"{nama_modif}-viral-vidioo-{random_string(6)}@gmail.com"
-        slug = f"New-Hot-video-{nama_modif}-Sex-Video-Viral-Hot-Soc𝚒al-Media-{random_string(6)}"
-        judul = f"(New Hot-video) {kw} Sex Video Viral Hot Soc𝚒al Media"
-        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25-7"
+        slug = f"{nama_modif}-Video-Original-Full-Video-Link-Maya-G-Viral-Video-Social-Media-X-Twitter-Trending-{random_string(6)}"
+        judul = f"[-ORIGINAL-]— * {kw} Video Original Full Video Link Maya G Viral Video Social Media X Twitter Trending"
+        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25-8"
 
         driver.get("https://bento.me/signup?ref=techcrunch&app=wetransferflow&atb=true")
         time.sleep(3)
