494b36008c7553109aad339857a3071314ff15d6
riponorel <riponorel@gmail.com>
2024-11-25 17:47:55 +0700

    Update run-t.py

--- run-t.py
+++ run-t.py
@@ -68,9 +68,9 @@ def run_bot(data_account, recover=1):
 
         nama_modif = kw.replace(" ", "-")
         gmail = f"{nama_modif}-viral-vidioo-{random_string(6)}@gmail.com"
-        slug = f"{nama_modif}-onlyfans-{random_string(6)}"
-        judul = f"[+[!STREAMING!]+] {kw} Onlyfans Leaked Video Viral On Social Media"
-        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25A"
+        slug = f"{nama_modif}-onlyfanss-{random_string(6)}"
+        judul = f"[~𝐄𝐗𝐂𝐋𝐔𝐒𝐈𝐕𝐄~]** {kw} Onlyfans Leaked Video Viral On Social Media Twitter x And Tiktok Trending Now"
+        link = f"https://apps.freeplayer.one/?title= CLICK HERE >> {kw}?ref=25B"
 
         driver.get("https://bento.me/signup?ref=techcrunch&app=wetransferflow&atb=true")
         time.sleep(3)
